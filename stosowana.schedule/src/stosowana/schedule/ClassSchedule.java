e0f67c70a192dc882451a6f4e63696a91793a8d2
whitecurrant <mcnadamczyk@gmail.com>
2014-02-20 23:45:36 +0100

    Teraz jest to widget!

---
+++ stosowana.schedule/src/stosowana/schedule/ClassSchedule.java
@@ -0,0 +1,14 @@
+package stosowana.schedule;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+ 
+public class ClassSchedule extends AppWidgetProvider {
+ 
+	 @Override
+     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    
+
+     }
+}
