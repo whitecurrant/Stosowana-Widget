0b320dcfb1edc7cc0813502c17d194444f22a0f6
Kefas <piotrkonsek@gmail.com>
2014-02-21 13:48:54 +0100

    Dodałem funkcję public void fetch(View) wywoływaną po naciśnięciu buttona. W niej pobieram login i hasło. Póki co tyle.

--- stosowana.schedule/src/stosowana/schedule/DataFetchActivity.java
+++ stosowana.schedule/src/stosowana/schedule/DataFetchActivity.java
@@ -2,6 +2,8 @@ package stosowana.schedule;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DataFetchActivity extends Activity {
 
@@ -15,5 +17,11 @@ public class DataFetchActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.data_fetch_layout);
 
+	}
+	
+	public void fetch(View view){
+		EditText mName = (EditText) findViewById(R.id.usernameField);
+		EditText mPasswd = (EditText) findViewById(R.id.passwdField);
+		
 	}
 }
