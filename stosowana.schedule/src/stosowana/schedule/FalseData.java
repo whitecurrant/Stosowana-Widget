4e96865f474fea55675e30de7f9c07690003ddac
whitecurrant <mcnadamczyk@gmail.com>
2014-02-25 21:29:56 +0100

    Improve False data

--- stosowana.schedule/src/stosowana/schedule/FalseData.java
+++ stosowana.schedule/src/stosowana/schedule/FalseData.java
@@ -10,11 +10,12 @@ public class FalseData {
 	private Map<Integer, List<Subject> > schedule; 
 	
 	public FalseData(){
+		
 		schedule = new HashMap<Integer, List<Subject>>();
 		for(int i =0;i<5;i++){
 			List<Subject> testList = new ArrayList<Subject>();
 			for(int j = 0;j<5;j++){
-				testList.add(new Subject("przemiot humanistyczny nr"+ i+j, "Mr", "123 B1"+j, "10:00", "11:30", Weekday.MON, true));
+				testList.add(new Subject("przemiot humanistyczny nr"+ i+j, "Mr", "123 B1"+j, "10:00", "11:30"));
 			
 				
 			} 
