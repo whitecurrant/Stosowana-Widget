62be1dc269899ebd04e8265b5c1c00a9dea57820
whitecurrant <mcnadamczyk@gmail.com>
2014-03-01 17:22:51 +0100

    Dodany scroller i time picker'y dla nowego przedmiotu

--- stosowana.schedule/gen/stosowana/schedule/R.java
+++ stosowana.schedule/gen/stosowana/schedule/R.java
@@ -14,6 +14,9 @@ public final class R {
         public static final int slide_right_in=0x7f040002;
         public static final int slide_right_out=0x7f040003;
     }
+    public static final class array {
+        public static final int days_array=0x7f090000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -51,50 +54,51 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_bar=0x7f0c000d;
-        public static final int action_bar_textview=0x7f0c0011;
-        public static final int action_settings=0x7f0c002b;
-        public static final int addSubject_day=0x7f0c0006;
-        public static final int addSubject_name=0x7f0c0001;
-        public static final int addSubject_room=0x7f0c0003;
-        public static final int addSubject_save=0x7f0c0007;
-        public static final int addSubject_start=0x7f0c0004;
-        public static final int addSubject_stop=0x7f0c0005;
-        public static final int addSubject_teacher=0x7f0c0002;
-        public static final int button1=0x7f0c0025;
-        public static final int button2=0x7f0c0020;
-        public static final int checkBox1=0x7f0c0022;
-        public static final int checkBox2=0x7f0c0023;
-        public static final int checkBox3=0x7f0c0024;
-        public static final int container=0x7f0c0017;
-        public static final int container0=0x7f0c0012;
-        public static final int container1=0x7f0c0013;
-        public static final int container2=0x7f0c0014;
-        public static final int container3=0x7f0c0015;
-        public static final int container4=0x7f0c0016;
-        public static final int empty_listView=0x7f0c000c;
-        public static final int empty_view=0x7f0c001e;
-        public static final int fetchDataBtn=0x7f0c000a;
-        public static final int fetchDataMenuButton=0x7f0c001f;
-        public static final int flipper=0x7f0c0018;
-        public static final int left_arrow_btn=0x7f0c000e;
-        public static final int listView=0x7f0c000b;
-        public static final int listView0=0x7f0c0019;
-        public static final int listView1=0x7f0c001a;
-        public static final int listView2=0x7f0c001b;
-        public static final int listView3=0x7f0c001c;
-        public static final int listView4=0x7f0c001d;
-        public static final int menu_button2=0x7f0c0021;
-        public static final int passwdField=0x7f0c0009;
-        public static final int right_arrow_btn=0x7f0c000f;
-        public static final int row_label=0x7f0c0028;
-        public static final int row_time=0x7f0c0026;
-        public static final int row_type=0x7f0c0027;
-        public static final int settings_bttn=0x7f0c0010;
-        public static final int textView1=0x7f0c0000;
-        public static final int textView2=0x7f0c0029;
-        public static final int textView3=0x7f0c002a;
-        public static final int usernameField=0x7f0c0008;
+        public static final int action_bar=0x7f0d000e;
+        public static final int action_bar_textview=0x7f0d0011;
+        public static final int action_settings=0x7f0d002c;
+        public static final int addSubject_name=0x7f0d0002;
+        public static final int addSubject_room=0x7f0d0004;
+        public static final int addSubject_save=0x7f0d0008;
+        public static final int addSubject_teacher=0x7f0d0003;
+        public static final int button1=0x7f0d0026;
+        public static final int button2=0x7f0d0021;
+        public static final int checkBox1=0x7f0d0023;
+        public static final int checkBox2=0x7f0d0024;
+        public static final int checkBox3=0x7f0d0025;
+        public static final int container=0x7f0d0018;
+        public static final int container0=0x7f0d0013;
+        public static final int container1=0x7f0d0014;
+        public static final int container2=0x7f0d0015;
+        public static final int container3=0x7f0d0016;
+        public static final int container4=0x7f0d0017;
+        public static final int day_spinner=0x7f0d0007;
+        public static final int empty_listView=0x7f0d000d;
+        public static final int empty_view=0x7f0d001f;
+        public static final int fetchDataBtn=0x7f0d000b;
+        public static final int fetchDataMenuButton=0x7f0d0020;
+        public static final int flipper=0x7f0d0019;
+        public static final int left_arrow_btn=0x7f0d000f;
+        public static final int listView=0x7f0d000c;
+        public static final int listView0=0x7f0d001a;
+        public static final int listView1=0x7f0d001b;
+        public static final int listView2=0x7f0d001c;
+        public static final int listView3=0x7f0d001d;
+        public static final int listView4=0x7f0d001e;
+        public static final int menu_button2=0x7f0d0022;
+        public static final int passwdField=0x7f0d000a;
+        public static final int right_arrow_btn=0x7f0d0010;
+        public static final int row_label=0x7f0d0029;
+        public static final int row_time=0x7f0d0027;
+        public static final int row_type=0x7f0d0028;
+        public static final int scrollView1=0x7f0d0000;
+        public static final int settings_bttn=0x7f0d0012;
+        public static final int start_time_btn=0x7f0d0005;
+        public static final int stop_time_btn=0x7f0d0006;
+        public static final int textView1=0x7f0d0001;
+        public static final int textView2=0x7f0d002a;
+        public static final int textView3=0x7f0d002b;
+        public static final int usernameField=0x7f0d0009;
     }
     public static final class integer {
         public static final int maxLoginLength=0x7f080000;
@@ -110,28 +114,28 @@ public final class R {
         public static final int test_layout=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f0b0000;
+        public static final int main=0x7f0c0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f090001;
-        public static final int addNewSubjectString=0x7f090012;
-        public static final int app_name=0x7f090000;
-        public static final int boxLabel=0x7f09000e;
-        public static final int check_box1=0x7f09000b;
-        public static final int check_box2=0x7f09000c;
-        public static final int check_box3=0x7f09000d;
-        public static final int dataFetchBtnTxt=0x7f090005;
-        public static final int emptyList=0x7f090008;
-        public static final int hint1=0x7f090003;
-        public static final int hint2=0x7f090004;
-        public static final int menu_button1=0x7f09000a;
-        public static final int menu_button2=0x7f090009;
-        public static final int save=0x7f09000f;
-        public static final int settings_bttn=0x7f090007;
-        public static final int test=0x7f090006;
-        public static final int test_type=0x7f090011;
-        public static final int time_mockup=0x7f090010;
-        public static final int widget_text=0x7f090002;
+        public static final int action_settings=0x7f0a0001;
+        public static final int addNewSubjectString=0x7f0a0012;
+        public static final int app_name=0x7f0a0000;
+        public static final int boxLabel=0x7f0a000e;
+        public static final int check_box1=0x7f0a000b;
+        public static final int check_box2=0x7f0a000c;
+        public static final int check_box3=0x7f0a000d;
+        public static final int dataFetchBtnTxt=0x7f0a0005;
+        public static final int emptyList=0x7f0a0008;
+        public static final int hint1=0x7f0a0003;
+        public static final int hint2=0x7f0a0004;
+        public static final int menu_button1=0x7f0a000a;
+        public static final int menu_button2=0x7f0a0009;
+        public static final int save=0x7f0a000f;
+        public static final int settings_bttn=0x7f0a0007;
+        public static final int test=0x7f0a0006;
+        public static final int test_type=0x7f0a0011;
+        public static final int time_mockup=0x7f0a0010;
+        public static final int widget_text=0x7f0a0002;
     }
     public static final class style {
         /** 
@@ -149,11 +153,11 @@ public final class R {
     
  API 11 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f0a0000;
+        public static final int AppBaseTheme=0x7f0b0000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f0a0001;
+        public static final int AppTheme=0x7f0b0001;
     }
     public static final class xml {
         public static final int widget_provider=0x7f050000;
--- stosowana.schedule/src/stosowana/schedule/AddNewSubjectActivity.java
+++ stosowana.schedule/src/stosowana/schedule/AddNewSubjectActivity.java
@@ -1,52 +1,95 @@
 package stosowana.schedule;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class AddNewSubjectActivity extends Activity {
+	
+	String name,teacher,room,start,stop;
+	int selectedDay;
+
+	public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			// Do something with the time chosen by the user
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_new_subject_layout);
+		Spinner spinner = (Spinner) findViewById(R.id.day_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.days_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+				selectedDay = pos;
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
 	}
-	
+	public void setStartTime(View v){
+		
+	}
+	public void setStopTime(View v){
+		
+	}
 	public void saveSubject(View view){
+		
 		Context context = this.getApplicationContext();
 		TextView mName = (TextView) findViewById(R.id.addSubject_name);
 		TextView mTeacher =(TextView) findViewById(R.id.addSubject_teacher);
 		TextView mRoom = (TextView)findViewById(R.id.addSubject_room);
-		TextView mStart =(TextView) findViewById(R.id.addSubject_start);
-		TextView mStop =(TextView) findViewById(R.id.addSubject_stop);
-		TextView mDay =(TextView) findViewById(R.id.addSubject_day);
+	
 		
-		String name = mName.getText().toString();
-		String teacher = mTeacher.getText().toString();
-		String room = mRoom.getText().toString();
-		String start = mStart.getText().toString();
-		String stop = mStop.getText().toString();
-		String day = mDay.getText().toString();
+		name = mName.getText().toString();
+		teacher = mTeacher.getText().toString();
+		room = mRoom.getText().toString();
+		//start = mStart.getText().toString();
+		//stop = mStop.getText().toString();
 		
-		if(name.isEmpty())
+		if(name == "")
 			Toast.makeText(context, "Proszę wpisać nazwę przedmiotu", Toast.LENGTH_SHORT).show();
-		else if(teacher.isEmpty())
+		else if(teacher == "")
 			Toast.makeText(context, "Proszę wpisać prowadzącego", Toast.LENGTH_SHORT).show();
-		else if(room.isEmpty())
+		else if(room == "")
 			Toast.makeText(context, "Proszę wpisać miejsce", Toast.LENGTH_SHORT).show();
-		else if(start.isEmpty())
-			Toast.makeText(context, "Proszę wpisać godzinę rozpoczęcia", Toast.LENGTH_SHORT).show();
-		else if(start.charAt(2) != ':' || start.length() !=5)
-			Toast.makeText(context, "Zły format daty rozpoczęcia", Toast.LENGTH_SHORT).show();
-		else if(stop.isEmpty())
-			Toast.makeText(context, "Proszę wpisać godzinę zakończenia", Toast.LENGTH_SHORT).show();
-		else if(stop.charAt(2) != ':' || stop.length() !=5)
-			Toast.makeText(context, "Zły format daty zakończenia", Toast.LENGTH_SHORT).show();
-		else if(day.isEmpty())
-			Toast.makeText(context, "Proszę wpisać dzień tygodnia", Toast.LENGTH_SHORT).show();
+	
 		else{
 			Subject subject = new Subject();
 			subject.setName(name);
@@ -55,10 +98,8 @@ public class AddNewSubjectActivity extends Activity {
 			subject.setStartTime(start);
 			subject.setStopTime(stop);
 			subject.setType("2");
-			Widget.add(Integer.parseInt(day), subject);
+			//Widget.add(Integer.parseInt("1"), subject);
 			finish();
 		}
 	}
-	
-
 }
