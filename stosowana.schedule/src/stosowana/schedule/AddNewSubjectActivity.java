ae37636e6522c0996e0e531556cb7b33ad0a2b74
whitecurrant <mcnadamczyk@gmail.com>
2014-03-01 23:25:54 +0100

    Bug (chyba) usunięty, weryfikacja czasu + ikonka

--- stosowana.schedule/src/stosowana/schedule/AddNewSubjectActivity.java
+++ stosowana.schedule/src/stosowana/schedule/AddNewSubjectActivity.java
@@ -2,6 +2,7 @@ package stosowana.schedule;
 
 import java.util.Calendar;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -19,38 +21,36 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+@SuppressLint("NewApi")
 public class AddNewSubjectActivity extends FragmentActivity {
 	
-	private String name,teacher,room;
+	private String name,teacher,room,startT,stopT;
 	int selectedDay;
 	private static String startTime = "0:00";
 	private static String stopTime = "0:00";
-	public static int startHour = 0;
-	public static int startMinute = 0;
 
 	public static class SetTimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
 		
-
+		static final Calendar c = Calendar.getInstance();
+		static int hour = c.get(Calendar.HOUR_OF_DAY);
+		static int minute = c.get(Calendar.MINUTE);
+		public static int chosenHour,chosenMinute;
+		
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
-		
-			final Calendar c = Calendar.getInstance();
-			int hour = c.get(Calendar.HOUR_OF_DAY);
-			int minute = c.get(Calendar.MINUTE);
+			
 			return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
 		}
 
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 			
-			startTime = hourOfDay+":"+minute;
-			startHour = hourOfDay;
-			startMinute = minute;
+			startTime = ((hourOfDay <10) ? "0" + hourOfDay : hourOfDay )+ ":"+ ((minute < 10) ? "0" + minute : minute) ;
 			TextView stTxt = (TextView) getActivity().findViewById(R.id.startTimeText);
 			stTxt.setText(startTime);
+			chosenHour = hourOfDay;
+			chosenMinute = minute;
 		}
-		
-		
 	}
 	public static class StopTimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
@@ -58,20 +58,24 @@ public class AddNewSubjectActivity extends FragmentActivity {
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			
-//			final Calendar c = Calendar.getInstance();
-//			int hour = c.get(Calendar.HOUR_OF_DAY);
-//			int minute = c.get(Calendar.MINUTE);
-			int hour = startHour + 1;
-			int minute = startMinute + 30;
+			int minute = SetTimePickerFragment.chosenMinute + 30;
+			int hour = SetTimePickerFragment.chosenHour + 1 ;
+			if(minute > 60)
+			{
+				minute -=60;
+				hour++;
+			}	
+			if(hour >= 24)
+				hour -=24;
+
 			return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
 		}
 
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 			
-			stopTime = hourOfDay+":"+minute;
+			stopTime = ((hourOfDay <10) ? "0" + hourOfDay  : hourOfDay )+ ":"+ ((minute < 10) ? "0" + minute : minute) ;
 			TextView stTxt = (TextView) getActivity().findViewById(R.id.stopTimeText);
 			stTxt.setText(stopTime);
-			
 		}
 	}
 	@Override
@@ -113,19 +117,26 @@ public class AddNewSubjectActivity extends FragmentActivity {
 		EditText mName = (EditText) findViewById(R.id.addSubject_name);
 		EditText mTeacher =(EditText) findViewById(R.id.addSubject_teacher);
 		EditText mRoom = (EditText)findViewById(R.id.addSubject_room);
+		TextView startText = (TextView)findViewById(R.id.startTimeText);
+		TextView stopText = (TextView)findViewById(R.id.stopTimeText);
 	
 		
 		name = mName.getText().toString();
 		teacher = mTeacher.getText().toString();
 		room = mRoom.getText().toString();
-//			nie mozna porowynwac == "", nie przynosilo pozadanego efektu
+		startT = startText.toString();
+		stopT = startText.toString();
+
 		if(name.isEmpty())
 			Toast.makeText(context, "Proszę wpisać nazwę przedmiotu", Toast.LENGTH_SHORT).show();
 		else if(teacher.isEmpty())
 			Toast.makeText(context, "Proszę wpisać prowadzącego", Toast.LENGTH_SHORT).show();
 		else if(room.isEmpty())
 			Toast.makeText(context, "Proszę wpisać miejsce", Toast.LENGTH_SHORT).show();
-//	czemu nie ma sprawdzenia daty oraz godziny?
+		else if(startT.isEmpty())
+			Toast.makeText(context, "Proszę wybrać godzinę zajęć", Toast.LENGTH_SHORT).show();
+		else if(stopT.isEmpty())	
+			Toast.makeText(context, "Proszę wybrać godzinę zajęć", Toast.LENGTH_SHORT).show();
 		else{
 			Subject subject = new Subject();
 			subject.setName(name);
--- stosowana.schedule/src/stosowana/schedule/RemoteViewsProvider.java
+++ stosowana.schedule/src/stosowana/schedule/RemoteViewsProvider.java
@@ -1,9 +1,7 @@
 package stosowana.schedule;
 import java.util.ArrayList;
-import java.util.List;
 
 import android.annotation.TargetApi;
-import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -26,26 +24,27 @@ public class RemoteViewsProvider implements RemoteViewsFactory {
 	@Override
 	public void onCreate() {
 		
-		Log.d("widget", "onCreate in InnerFactory for day  " + intent.getIntExtra("dayNum", -1));
+//		Log.d("widget", "onCreate in InnerFactory for day  " + intent.getIntExtra("dayNum", -1));
 	
 	} 
 	@Override
 	public int getCount() {
 		
-		Log.d("widget", "calling getCount for day " + intent.getIntExtra("dayNum", -1));
+/*		Log.d("widget", "calling getCount for day " + intent.getIntExtra("dayNum", -1));
 		
 		if(itemList == null)
 			Log.d("widget", "list empty in getCount for day  "+intent.getIntExtra("dayNum", -1));
 		else
-			Log.d("widget", "getCount returns " + itemList.size() +" for day " + intent.getIntExtra("dayNum", -1));
+			Log.d("widget", "getCount returns " + itemList.size() +" for day " + intent.getIntExtra("dayNum", -1));*/
 			
-		
+		if(itemList == null)
+			return 0;
 		return itemList.size();
 	}
 	 
 	@Override
 	public long getItemId(int position) {
-		Log.d("widget", "getting itemId " + position + " for day " + intent.getIntExtra("dayNum", -1));
+//		Log.d("widget", "getting itemId " + position + " for day " + intent.getIntExtra("dayNum", -1));
 		return position;
 	}
 
@@ -53,14 +52,17 @@ public class RemoteViewsProvider implements RemoteViewsFactory {
 	public RemoteViews getViewAt(int position) {
 		
 		RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.row_layout);
-		Log.d("widget", "getting item "+position+" in getView for day " + intent.getIntExtra("dayNum", -1));
+//		Log.d("widget", "getting item "+position+" in getView for day " + intent.getIntExtra("dayNum", -1));
 
-		Subject item = itemList.get(position);
-		remoteView.setTextViewText(R.id.row_time, item.getStartTime() + " - " + item.getStopTime());
-		remoteView.setTextViewText(R.id.row_type, item.getType().toString());
-		remoteView.setTextViewText(R.id.row_label, item.toString());
-		Log.d("widget", "returning ListView for " + intent.getIntExtra("dayNum", -1));
-
+		if(position < getCount()){
+			Subject item = itemList.get(position);
+			remoteView.setTextViewText(R.id.row_time, item.getStartTime() + " - " + item.getStopTime());
+			remoteView.setTextViewText(R.id.row_type, item.getType().toString());
+			remoteView.setTextViewText(R.id.row_label, item.toString());
+//			Log.d("widget", "returning ListView for " + intent.getIntExtra("dayNum", -1));
+		}
+		else
+			Log.d("widget", "returning empty view");
 		return remoteView;
 	}
 
@@ -81,25 +83,25 @@ public class RemoteViewsProvider implements RemoteViewsFactory {
 	@Override
 	public boolean hasStableIds() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public void onDataSetChanged() {
 		
-		Log.d("widget","calling onDataSetChanged for day "+intent.getIntExtra("dayNum", -1));		
+//		Log.d("widget","calling onDataSetChanged for day "+intent.getIntExtra("dayNum", -1));		
 		int dayNum = intent.getIntExtra("dayNum",0);
 		if(Widget.getSchedule() == null){
 			Log.d("widget", "list Empty in OndataSetChanged for day  "+intent.getIntExtra("dayNum", -1));
 			return;
 		}
 		itemList = (ArrayList<Subject>) Widget.subjectSieve(Widget.getSchedule().get(dayNum));
-		Log.d("widget", "current list" + itemList.toString());
+//		Log.d("widget", "current list" + itemList.toString());
 	}
 
 	@Override
 	public void onDestroy() {
-		Log.d("widget", "destroying factory for day  "+intent.getIntExtra("dayNum", -1));
+//		Log.d("widget", "destroying factory for day  "+intent.getIntExtra("dayNum", -1));
 		// TODO Auto-generated method stub
 		
 	}
